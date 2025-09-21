8c13f0a1786bc7ddffe67989a877db450b5cbabb
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-09-21 19:42:15 +0000

    Deepfake detector

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+import Footer from "./footer";
+import Navbar from "./navbar";
+import UploadFile from "./uploadFile";
+import Usage from "./usage";
+import Feature from "./feature";
+import ParticlesBg from "particles-bg";
+import Title from "./title";
+import livenessImage from "./Image/liveness.png"; // Adjust the path as needed
+
+function App() {
+  const [showUpload, setShowUpload] = useState(false);
+
+  const handleStartDetection = () => {
+    setShowUpload(true);
+    document
+      .getElementById("upload-file-wrap")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="App">
+      <div id="nav-main-wrap">
+        <Navbar onStartDetection={handleStartDetection} />
+
+        <main id="main-section">
+          <div id="demo-uploadfile-wrap">
+            <Title onStartDetection={handleStartDetection} />
+            <img src={livenessImage} alt="Liveness Detection" />
+          </div>
+        </main>
+      </div>
+      {showUpload && <UploadFile />}
+
+      <Usage />
+      <Feature />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
